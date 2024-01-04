b6975c0942a7af15b881de3e5a8a875a2515ae3b
Florian Daude <floriandaude@hotmail.fr>
2024-01-05 00:27:16 +0100

    added ApiKey config in __init__

--- oci_client/__init__.py
+++ oci_client/__init__.py
@@ -1,2 +1,2 @@
-from oci_client.cfg import Config  # noqa: F401 # pyright: ignore[reportUnusedImport]
+from oci_client.cfg import ApiKey, Config  # noqa: F401 # pyright: ignore[reportUnusedImport]
 from oci_client.client import Client  # noqa: F401 # pyright: ignore[reportUnusedImport]
